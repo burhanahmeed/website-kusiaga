757d2ccb6c5031cffbbe7f108244b23ebacc343a
burhan ahmed <burhanahmeed@gmail.com>
2020-04-25 14:13:19 +0700

    add cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,5 +8,35 @@ module.exports = {
   siteName: 'Kusiaga',
   titleTemplate: '%s - Digital Experiement Lab',
   icon: './src/assets/icon.jpg',
-  plugins: []
+
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
 }
