318a25e3848ffe0909b6439426e7f2668dbd87ed
burhan ahmed <burhanahmeed@gmail.com>
2020-08-12 21:41:47 +0700

    update talks page

--- src/pages/Talks.vue
+++ src/pages/Talks.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <h4>My Talks</h4>
+    <!-- <h4>My Talks</h4>
     <ul>
         <li>
             <div class="card">
@@ -11,12 +11,52 @@
                 <span>Date: 16 May 2020</span>
             </div>
         </li>
-    </ul>
+    </ul> -->
+    <c-box w="100%">
+      <c-box py="4">
+        <c-text fontSize="xl" fontWeight="700">Talks</c-text>
+      </c-box>
+      <c-box w="100%" bg="#a6a6a614" p="1">
+        <c-box borderBottom="thin #00000017 solid" p="5">
+          <c-link href="https://speakerdeck.com/burhanahmeed/javascript-and-seo-so" is-external>
+            <c-text fontSize="xl" fontWeight="700"><c-icon name="arrow-forward"></c-icon> Javascript and SEO, So?</c-text> 
+          </c-link>
+          <c-text fontSize="xl">SurabayaJS Online Meetup</c-text>
+          <c-box d="flex">
+            <c-box mr="5">
+              <c-text>
+                16 May 2020
+              </c-text>
+            </c-box>
+            <c-box mr="5">
+              <c-text>
+                Surabaya, Indonesia
+              </c-text>
+            </c-box>
+          </c-box>
+        </c-box>
+      </c-box>
+    </c-box>
   </Layout>
 </template>
 
 <script>
+import { 
+    CText, 
+    CButton,
+    CBox,
+    CLink,
+    CIcon,
+    CImage,
+    CFlex
+} from "@chakra-ui/vue";
 export default {
+  components: {
+    CBox,
+    CLink,
+    CIcon,
+    CText
+  },
   metaInfo (){
     return {
       title: 'My Talks',
